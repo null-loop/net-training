51b6c3e10d276698f362cf4e2617462799db01b9
Daniel Gray <null.loop@gmail.com>
2018-01-19 16:46:37 +0000

    Added palindrome test structure.

---
+++ FunctionTesting/FunctionTesting/PalindromeTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Functions;
+using Xunit;
+
+namespace FunctionTesting
+{
+    public class PalindromeTests
+    {
+        [Theory]
+        [InlineData("radar", true)]
+        [InlineData("anna", true)]
+        [InlineData("Civic", true)]
+        [InlineData("DEVOVED", true)]
+        [InlineData("Aibohphobia", true)]
+        [InlineData("lard", false)]
+        [InlineData("Palindrome", false)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        public void IsSimplePalindrome(string text, bool isPalindrome)
+        {
+            Palindromes.IsPalindrome(text).Should().Be(isPalindrome);
+        }
+
+        [Theory]
+        [InlineData("Do nine men interpret? Nine men. I nod.", true)]
+        [InlineData("A dog! A panic in a pagoda!", true)]
+        [InlineData("A Toyota! Race fast, safe car! A Toyota!", true)]
+        [InlineData("Are we not pure? \"No sir!\" Panama’s moody Noriega brags. \"It is garbage!\" Irony dooms a man; a prisoner up to new era.", true)]
+        [InlineData("Not a palindrome", false)]
+        public void IsAdvancedPalindrome(string text, bool isPalindrome)
+        {
+            Palindromes.IsPalindrome(text).Should().Be(isPalindrome);
+        }
+    }
+}
---
+++ FunctionTesting/Functions/Palindromes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    public static class Palindromes
+    {
+        public static bool IsPalindrome(string text)
+        {
+            //TODO:Implement me!
+            throw new NotImplementedException();
+        }
+    }
+}
