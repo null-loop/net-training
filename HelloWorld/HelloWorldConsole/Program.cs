6ff2c85e72dc5ccf6488112126c2f8e20e4569fb
Daniel Gray <null.loop@gmail.com>
2018-01-19 11:57:58 +0000

    Added hello world code from first demo

---
+++ HelloWorld/HelloWorldConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using HelloWorldLibrary;
+using System;
+
+namespace HelloWorldConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stringToPrint = HelloWorldNamer.GetHelloWorld(args);
+
+            Console.WriteLine(stringToPrint);
+        }
+    }
+}
---
+++ HelloWorld/HelloWorldLibrary/HelloWorldNamer.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloWorldLibrary
+{
+    public class HelloWorldNamer
+    {
+        public static string GetHelloWorld(string[] names)
+        {
+            if (names.Length == 0)
+            {
+                return "Hello World!";
+            }
+
+            var allNames = string.Join(" and ", names);
+
+            return $"Hello {allNames}!";
+
+        }
+    }
+}
