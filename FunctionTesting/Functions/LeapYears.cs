50555c5a40ff6139b998ddc2b31100166c51e6f4
Daniel Gray <null.loop@gmail.com>
2018-01-22 09:46:01 +0000

    Added leap year tests.

---
+++ FunctionTesting/Functions/LeapYears.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Functions
+{
+    public static class LeapYears
+    {
+        public static bool IsLeapYear(int year)
+        {
+            /*There is a leap year every year whose number is perfectly divisible by four - 
+             * except for years which are both divisible by 100 and not divisible by 400. 
+             * The second part of the rule effects century years. 
+             * For example; the century years 1600 and 2000 are leap years, 
+             * but the century years 1700, 1800, and 1900 are not.*/
+            //TOOD:Implement me!
+            throw new NotImplementedException();
+        }
+    }
+}
